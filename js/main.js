4b249a3e9c9ac324d6ca194984dd9f43f8ecba02
ckuji <victor02guygo@mail.ru>
2020-11-16 10:39:16 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.nav__burg-wrapper').click(function () {
+    $('.nav__items').toggleClass('nav__items--show');
+    $('.nav__burg-top').toggleClass('nav__burg-top--active');
+    $('.nav__burg-mid').toggleClass('nav__burg-mid--active');
+    $('.nav__burg-bot').toggleClass('nav__burg-bot--active');
+});
+
+
